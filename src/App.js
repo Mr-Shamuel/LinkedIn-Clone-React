5138b0d566ac7b8ea5ab0ac3f514d196ae434aa7
Mr-Shamuel <shamuelsam300@gmail.com>
2023-02-16 01:17:08 +0600

    pre loader added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import Home from "./Pages/HomePage/Home/Home";
 import NotFound from "./Components/NotFound/NotFound";
 import SignIn from "./Pages/Sign In page/SignIn";
 import SignUp from "./Pages/Sign Up/SignUp";
+import { useEffect, useState } from "react";
+import Preloading from "./Components/Loding/PreLoading/Preloading";
 
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
 
 
-      <Routes>
+      {isLoading ? <Preloading></Preloading>
+        : (<Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>)}
+
+      {/* <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='*' element={<NotFound />} />
-      </Routes>
+      </Routes> */}
 
 
     </div>
---
+++ src/Components/Loding/PreLoading/Preloading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Preloading.css'
+const Preloading = () => {
+    return (
+        <div className='preLoadingcon'>
+            <div class="container">
+                <div class="part1">
+                    Linked
+                </div>
+                <div class="part2">
+                    in
+                </div>
+            </div>
+
+
+            <div class="loader">
+                <div class="loading">
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Preloading;
