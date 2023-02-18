ef2cd8c02def25bddee8197a196e1707f6cd89f0
Md Shamuel Molla <Mr-Shamuel>
2023-02-19 01:34:58 +0600

    wow

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <h1>hello</h1>
+    <h1>hello </h1>
     <App />
   </BrowserRouter>
 
