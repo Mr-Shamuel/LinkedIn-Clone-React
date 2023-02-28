0a42fdaa3412bdd7e9033149840ddb44aced39dc
Md Shamuel Molla <Mr-Shamuel>
2023-02-28 14:01:56 +0600

    update successfully

--- src/Components/Modal/Privacy Modal/PrivacyModal.js
+++ src/Components/Modal/Privacy Modal/PrivacyModal.js
@@ -1,10 +1,12 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './PrivacyModal.css'
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const PrivacyModal = ({ handlePrivecy, id }) => {
-    console.log(id)
+
     const [privacy, setPrivacy] = useState('');
     const navigate = useNavigate()
 
@@ -15,27 +17,38 @@ const PrivacyModal = ({ handlePrivecy, id }) => {
 
         })
             .then(() => {
-                console.log('success')
                 navigate('/home')
+
+                handlePrivecy()
 
 
             })
             .catch(error => {
                 console.error('Error updating user', error);
             })
-        handlePrivecy()
+
+
+
+        toast.success('Privacy Updated', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
         e.preventDefault();
     }
-    useEffect(() => {
-        console.log(privacy)
-    }, [privacy])
+
     return (
         <div className='privacyCon modalCon'>
             {(<div className="modal">
                 <div className="overlay" onClick={handlePrivecy}></div>
                 <div className="modal-content">
                     <div className="modal_header">
-                        <h6>Change Privacy</h6>
+                        <h6>Change Privacy </h6>
 
                         <div className='close-modal' onClick={handlePrivecy}> <svg style={{ width: '18px', height: '18px' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match" focusable="false">
                             <path d="M13.42 12L20 18.58 18.58 20 12 13.42 5.42 20 4 18.58 10.58 12 4 5.42 5.42 4 12 10.58 18.58 4 20 5.42z"></path>
@@ -62,7 +75,12 @@ const PrivacyModal = ({ handlePrivecy, id }) => {
                         </div>
 
 
-                        <button type="submit">Submit</button>
+                        {/* <button type="submit">Save</button> */}
+
+                        {privacy ? <button type="submit">Save</button>
+                            : <button disabled style={{ backgroundColor: 'rgb(191, 194, 191)', color: 'black', border: '1px solid grey', cursor: 'not-allowed' }} type="submit">Save</button>
+                        }
+
                     </form>
 
 
@@ -77,7 +95,20 @@ const PrivacyModal = ({ handlePrivecy, id }) => {
             </div>)}
 
 
-
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            {/* Same as */}
+            <ToastContainer />
 
 
         </div>
